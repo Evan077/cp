1fa9a14bbb767b41c119f696a64d14bf2a49e9ee
Evan077 <113843628+Evan077@users.noreply.github.com>
2025-04-27 12:03:18 +0600

    Create sparse_table.cpp

---
+++ sparse_table.cpp
@@ -0,0 +1,20 @@
+struct spr
+{
+  ll n;
+  int b=20;
+    vector<vector<ll>>v;
+    void in_it(ll _n){
+       this->n=_n;
+       v.resize(n+1);
+       for(int i=0;i<=n;i++){
+         v[i].resize(b+1,0);
+       }
+    }
+    void build(ll a[]){
+       for(int i=1;i<=n;i++){v[i][0]=a[i];}
+      for(int k=1;k<=b;k++){
+          for(int i=1;i+(1<<k)-1<=n;i++){
+              v[i][k]=min(v[i][k-1],v[i+(1<<(k-1))][k-1]);
+          }
+      }
+}
