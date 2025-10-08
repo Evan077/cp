86f83044f768ab2c9a5724ff1d6f6ddf63c5cc44
MD. Abu Evan Payel <113843628+Evan077@users.noreply.github.com>
2025-10-08 18:17:08 +0600

    Improve hashing function in PairHash struct
    Refactor PairHash operator() for improved hashing of pairs.

--- hashing_in_unordered_map_and_set.cpp
+++ hashing_in_unordered_map_and_set.cpp
@@ -1,19 +1,11 @@
 struct PairHash {
-    size_t operator()(const pair<ll,ll>& p) const noexcept {
-        return std::hash<ll>()(p.first) ^ (std::hash<ll>()(p.second) << 1);
-    }
+  size_t operator()(const pair<ll, ll>& p) const noexcept {
+    size_t h1 = hash<ll>{}(p.first);
+    size_t h2 = hash<ll>{}(p.second);
+    return h1 ^ (h2 + 0x9e3779b97f4a7c15ULL + (h1 << 6) + (h1 >> 2));
+  }
 };
 // this is how to declare unordered maps
 // unordered_map<pair<ll,ll>,pair<ll,ll>,PairHash>m;
  // unordered_map<pair<ll,ll>, ll, PairHash> m;
-
-
-//for unorderes_set
-struct PairHash {
-    size_t operator()(const pair<ll,ll>& p) const noexcept {
-        size_t h1 = std::hash<ll>{}(p.first);
-        size_t h2 = std::hash<ll>{}(p.second);
-        return h1 ^ (h2 + 0x9e3779b97f4a7c15ULL + (h1<<6) + (h1>>2));
-    }
-};
-//unordered_set<pair<int,int>,PairHash>st;
+// also works for un_ordered set;
